f3640aa54c179777b5bf5ab4cb515864a719fbe9
rodriguez-luis <pb.rndm@gmail.com>
2021-04-13 12:55:15 -0400

    add location controller

---
+++ src/main/java/com/ucb/Nexo_Backend/controllers/LocationController.java
@@ -0,0 +1,19 @@
+package com.ucb.Nexo_Backend.controllers;
+
+import com.ucb.Nexo_Backend.models.Location;
+import com.ucb.Nexo_Backend.services.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class LocationController {
+    @Autowired
+    private LocationService service;
+    @GetMapping(value = "/bol")
+    public List<Location> getAll(){
+        return this.service.getAll();
+    }
+}
