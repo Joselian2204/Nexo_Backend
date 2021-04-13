deb260d88757b02d2d478bd558086b53c4821414
rodriguez-luis <pb.rndm@gmail.com>
2021-04-13 12:51:51 -0400

    add location model

---
+++ src/main/java/com/ucb/Nexo_Backend/models/Location.java
@@ -0,0 +1,45 @@
+package com.ucb.Nexo_Backend.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String name;
+
+    public Location() {
+    }
+    public Location(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
