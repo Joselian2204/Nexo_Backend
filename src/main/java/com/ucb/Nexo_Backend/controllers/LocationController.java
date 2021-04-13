76a4a8c9272d389dc6bc15e1fcba91d22cb34193
Joselian2204 <josemar.elian.castro.paz@gmail.com>
2021-04-13 16:17:22 -0400

    LocationController - CrossOrigin

--- src/main/java/com/ucb/Nexo_Backend/controllers/LocationController.java
+++ src/main/java/com/ucb/Nexo_Backend/controllers/LocationController.java
@@ -3,11 +3,13 @@ package com.ucb.Nexo_Backend.controllers;
 import com.ucb.Nexo_Backend.models.Location;
 import com.ucb.Nexo_Backend.services.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 public class LocationController {
     @Autowired
@@ -17,3 +19,4 @@ public class LocationController {
         return this.service.getAll();
     }
 }
+
