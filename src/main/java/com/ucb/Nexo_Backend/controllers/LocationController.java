c37a8edb356ac0d75e7a6f87a3f2624a3d515187
rodriguez-luis <pb.rndm@gmail.com>
2021-04-14 21:11:39 -0400

    add department model

--- src/main/java/com/ucb/Nexo_Backend/controllers/LocationController.java
+++ src/main/java/com/ucb/Nexo_Backend/controllers/LocationController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class LocationController {
     @Autowired
     private LocationService service;
-    @GetMapping(value = "/bol")
+    @GetMapping(value = "/loc")
     public List<Location> getAll(){
         return this.service.getAll();
     }
---
+++ src/main/java/com/ucb/Nexo_Backend/models/Department.java
@@ -0,0 +1,94 @@
+package com.ucb.Nexo_Backend.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Department {
+    @Id
+    @GeneratedValue
+    @Column(name = "id_department")
+    private String departmentId;
+    @Column(name = "id_country")
+    private String countryId;
+    private String name;
+    private String population;
+    @Column(name = "year_census")
+    private String censusYear;
+    private String latitud;
+    private String longitud;
+
+    public Department() {
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "departmentId='" + departmentId + '\'' +
+                ", countryId='" + countryId + '\'' +
+                ", name='" + name + '\'' +
+                ", population='" + population + '\'' +
+                ", censusYear='" + censusYear + '\'' +
+                ", latitud='" + latitud + '\'' +
+                ", longitud='" + longitud + '\'' +
+                '}';
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(String countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(String population) {
+        this.population = population;
+    }
+
+    public String getCensusYear() {
+        return censusYear;
+    }
+
+    public void setCensusYear(String censusYear) {
+        this.censusYear = censusYear;
+    }
+
+    public String getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(String latitud) {
+        this.latitud = latitud;
+    }
+
+    public String getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(String longitud) {
+        this.longitud = longitud;
+    }
+}
