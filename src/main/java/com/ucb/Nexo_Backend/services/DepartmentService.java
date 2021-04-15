59dce777ecbd79da1bd1976f6952078e5ddb5222
rodriguez-luis <pb.rndm@gmail.com>
2021-04-14 21:14:08 -0400

    add department service

---
+++ src/main/java/com/ucb/Nexo_Backend/services/DepartmentService.java
@@ -0,0 +1,21 @@
+package com.ucb.Nexo_Backend.services;
+
+import com.ucb.Nexo_Backend.models.Department;
+import com.ucb.Nexo_Backend.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentService {
+    private DepartmentRepository repo;
+    @Autowired
+    public void setService(DepartmentRepository repo){
+        this.repo = repo;
+    }
+
+    public List<Department> getAll(){
+        return repo.findAll();
+    }
+}
