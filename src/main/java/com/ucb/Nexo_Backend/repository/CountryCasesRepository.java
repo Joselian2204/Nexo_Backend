7c5f10f134f947988755b0c83ab7ed2389b83111
rodriguez-luis <pb.rndm@gmail.com>
2021-04-15 20:39:38 -0400

    add CountryCases repository

---
+++ src/main/java/com/ucb/Nexo_Backend/repository/CountryCasesRepository.java
@@ -0,0 +1,10 @@
+package com.ucb.Nexo_Backend.repository;
+
+import com.ucb.Nexo_Backend.models.CountryCases;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CountryCasesRepository extends JpaRepository<CountryCases, Integer> {
+    public List<CountryCases> findByCountryId(String id);
+}
