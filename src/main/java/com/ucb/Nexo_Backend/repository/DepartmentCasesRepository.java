36e272b91c79fa53ce33a51a1caf925c01794caf
rodriguez-luis <pb.rndm@gmail.com>
2021-04-15 20:54:51 -0400

    add DepartmentCases Repository

---
+++ src/main/java/com/ucb/Nexo_Backend/repository/DepartmentCasesRepository.java
@@ -0,0 +1,10 @@
+package com.ucb.Nexo_Backend.repository;
+
+import com.ucb.Nexo_Backend.models.DepartmentCases;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DepartmentCasesRepository extends JpaRepository<DepartmentCases, Integer> {
+    public List<DepartmentCases> findByDepartmentId(String id);
+}
