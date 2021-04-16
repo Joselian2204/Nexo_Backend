4d79383b918e8039f99d025f802ebe2a45719999
rodriguez-luis <pb.rndm@gmail.com>
2021-04-15 20:42:01 -0400

    update country controller

--- src/main/java/com/ucb/Nexo_Backend/controllers/CountryController.java
+++ src/main/java/com/ucb/Nexo_Backend/controllers/CountryController.java
@@ -1,9 +1,12 @@
 package com.ucb.Nexo_Backend.controllers;
 
 import com.ucb.Nexo_Backend.models.Country;
+import com.ucb.Nexo_Backend.models.CountryCases;
+import com.ucb.Nexo_Backend.services.CountryCasesService;
 import com.ucb.Nexo_Backend.services.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,12 +14,18 @@ import java.util.List;
 @RestController
 public class CountryController {
     private CountryService service;
+    private CountryCasesService casesService;
     @Autowired
-    public void setService(CountryService service){
+    public void setServices(CountryService service, CountryCasesService casesService){
         this.service = service;
+        this.casesService = casesService;
     }
     @GetMapping(value = "/world")
     public List<Country> getCountries(){
         return service.getAll();
+    }
+    @GetMapping(value = "/country/{id}")
+    public List<CountryCases> getCases(@PathVariable String id){
+        return casesService.getByCountryId(id);
     }
 }
