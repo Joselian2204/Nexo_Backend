5d09b5b20f1c91e5c5ac9ba70859c62a352a39b2
Joselian2204 <josemar.elian.castro.paz@gmail.com>
2021-04-17 19:29:31 -0400

    CrossOrigin Municipio Added

--- src/main/java/com/ucb/Nexo_Backend/controllers/MunicipioController.java
+++ src/main/java/com/ucb/Nexo_Backend/controllers/MunicipioController.java
@@ -6,12 +6,14 @@ import com.ucb.Nexo_Backend.models.MunicipioCases;
 import com.ucb.Nexo_Backend.services.MunicipioCasesService;
 import com.ucb.Nexo_Backend.services.MunicipioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 public class MunicipioController {
     private MunicipioService service;
