5dc7200cce2510da727c51da5f208b461d0015ef
Silvana <silvana.munoz.vargas@gmail.com>
2021-04-18 23:46:37 -0400

    Creacion triggers de despues de la eliminacion de un dato

--- SQL/triggers.sql
+++ SQL/triggers.sql
@@ -1,16 +1,30 @@
 drop trigger if  exists tg_insert_h_day_country ;
 drop trigger if  exists tg_update_h_day_country;
 drop trigger if  exists tg_insert_h_day_department ;
+
 drop trigger if  exists tg_update_h_day_department;
 drop trigger if  exists tg_insert_h_day_municipality ;
 drop trigger if  exists tg_update_h_day_municipality;
+
+drop trigger if  exists tg_update_total_department;
+drop trigger if  exists tg_update_total_country;
+drop trigger if  exists tg_update_total_municipality;
+
+drop trigger if  exists tg_update_total_department;
+drop trigger if  exists tg_update_total_country;
+drop trigger if  exists tg_update_total_municipality;
+
+drop trigger if  exists c;
+drop trigger if  exists tg_update_delete_total_country;
+drop trigger if  exists tg_update_delete_total_municipality;
+
 
 DELIMITER |
 CREATE TRIGGER tg_insert_h_day_country
 AFTER INSERT ON day_country
 FOR EACH ROW
 BEGIN
-	INSERT INTO `h_day_country`(`id_day_country`,`id_country`,`date`,`new_cases`,`actives`,`deaths`,`recovered`,`vaccine`,`tx_date`,`tx_host`,`tx_update`) VALUES ( NEW.id_day_country,NEW.id_country,NEW.date,NEW.new_cases,NEW.actives,NEW.deaths,NEW.recovered,NEW.vaccine,NOW(),NEW.tx_host,NOW());
+	INSERT INTO `h_day_country`(`id_day_country`,`id_country`,`date`,`new_cases`,`actives`,`deaths`,`recovered`,`vaccine`,`tx_date`,`tx_host`,`tx_update`) VALUES ( NEW.id_day_country,NEW.id_country,NEW.date,NEW.cases,NEW.actives,NEW.deaths,NEW.recovered,NEW.vaccine,NOW(),NEW.tx_host,NOW());
 END;
 |
 DELIMITER ;
@@ -20,7 +34,7 @@ CREATE TRIGGER tg_update_h_day_country
 AFTER UPDATE ON day_country
 FOR EACH ROW
 BEGIN
-	INSERT INTO `h_day_country`(`id_day_country`,`id_country`,`date`,`new_cases`,`actives`,`deaths`,`recovered`,`vaccine`,`tx_date`,`tx_host`,`tx_update`) VALUES ( NEW.id_day_country,NEW.id_country,NEW.date,NEW.new_cases,NEW.actives,NEW.deaths,NEW.recovered,NEW.vaccine,NOW(),NEW.tx_host,NOW());
+	INSERT INTO `h_day_country`(`id_day_country`,`id_country`,`date`,`new_cases`,`actives`,`deaths`,`recovered`,`vaccine`,`tx_date`,`tx_host`,`tx_update`) VALUES ( NEW.id_day_country,NEW.id_country,NEW.date,NEW.cases,NEW.actives,NEW.deaths,NEW.recovered,NEW.vaccine,NOW(),NEW.tx_host,NOW());
 END;
 |
 DELIMITER ;
@@ -97,6 +111,41 @@ BEGIN
     UPDATE country
     SET cases = cases + new.new_cases, deaths = deaths + new.deaths, recovered = recovered + new.recovered, vaccine = vaccine + new.vaccine
     WHERE id_municipality= new.id_municipality;
+END;
+|
+DELIMITER ;
+
+
+DELIMITER |
+CREATE TRIGGER  tg_update_delete_total_department
+    AFTER delete ON day_department
+    FOR EACH ROW
+BEGIN
+    UPDATE department
+    SET cases = cases - OLD.new_cases, deaths = deaths - OLD.deaths, recovered = recovered - OLD.recovered, vaccine = vaccine - OLD.vaccine
+    WHERE id_department = OLD.id_department;
+END;
+|
+DELIMITER ;
+
+DELIMITER |
+CREATE TRIGGER  tg_update_delete_total_country
+    AFTER delete ON day_country
+    FOR EACH ROW
+BEGIN
+    UPDATE country
+    SET cases = cases - OLD.cases, deaths = deaths - OLD.deaths, recovered = recovered - OLD.recovered, vaccine = vaccine - OLD.vaccine
+    WHERE id_country = OLD.id_country;
+END;
+|
+DELIMITER ;
+CREATE TRIGGER  tg_update_delete_total_municipality
+    AFTER delete ON day_municipality
+    FOR EACH ROW
+BEGIN
+    UPDATE country
+    SET cases = cases - OLD.new_cases, deaths = deaths - OLD.deaths, recovered = recovered - OLD.recovered, vaccine = vaccine - OLD.vaccine
+    WHERE id_municipality= OLD.id_municipality;
 END;
 |
 DELIMITER ;
