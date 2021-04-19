a3355b428c71d70d4e3c1b7d792f035d525bdc2d
Silvana <silvana.munoz.vargas@gmail.com>
2021-04-18 20:34:55 -0400

    Actualizacion y correcion de municipality

--- SQL/triggers.sql
+++ SQL/triggers.sql
@@ -2,8 +2,8 @@ drop trigger if  exists tg_insert_h_day_country ;
 drop trigger if  exists tg_update_h_day_country;
 drop trigger if  exists tg_insert_h_day_department ;
 drop trigger if  exists tg_update_h_day_department;
-drop trigger if  exists tg_insert_h_day_municipio ;
-drop trigger if  exists tg_update_h_day_municipio;
+drop trigger if  exists tg_insert_h_day_municipality ;
+drop trigger if  exists tg_update_h_day_municipality;
 
 DELIMITER |
 CREATE TRIGGER tg_insert_h_day_country
@@ -45,29 +45,31 @@ END;
 |
 DELIMITER ;
 
+USE covid_db_test;
+
 DELIMITER |
-CREATE TRIGGER tg_insert_h_day_municipio
-AFTER INSERT ON day_municipio
+CREATE TRIGGER tg_insert_h_day_municipality
+AFTER INSERT ON day_municipality
 FOR EACH ROW
 BEGIN
-	INSERT INTO `h_day_municipio`(`id_day_municipio`,`id_municipio`,`date`,`new_cases`,`actives`,`deaths`,`recovered`,`vaccine`,`tx_date`,`tx_host`,`tx_update`) VALUES ( NEW.id_day_municipio,NEW.id_municipio,NEW.date,NEW.new_cases,NEW.actives,NEW.deaths,NEW.recovered,NEW.vaccine,NOW(),NEW.tx_host,NOW());
+	INSERT INTO `h_day_municipality`(`id_day_municipality`,`id_municipality`,`date`,`new_cases`,`actives`,`deaths`,`recovered`,`vaccine`,`tx_date`,`tx_host`,`tx_update`) VALUES ( NEW.id_day_municipality,NEW.id_municipality,NEW.date,NEW.new_cases,NEW.actives,NEW.deaths,NEW.recovered,NEW.vaccine,NOW(),NEW.tx_host,NOW());
 END;
 |
 DELIMITER ;
 
 DELIMITER |
-CREATE TRIGGER tg_update_h_day_municipio
-AFTER UPDATE ON day_municipio
+CREATE TRIGGER tg_update_h_day_municipality
+AFTER UPDATE ON day_municipality
 FOR EACH ROW
 BEGIN
-	INSERT INTO `h_day_municipio`(`id_day_municipio`,`id_municipio`,`date`,`new_cases`,`actives`,`deaths`,`recovered`,`vaccine`,`tx_date`,`tx_host`,`tx_update`) VALUES ( NEW.id_day_municipio,NEW.id_municipio,NEW.date,NEW.new_cases,NEW.actives,NEW.deaths,NEW.recovered,NEW.vaccine,NOW(),NEW.tx_host,NOW());
+	INSERT INTO `h_day_municipality`(`id_day_municipality`,`id_municipality`,`date`,`new_cases`,`actives`,`deaths`,`recovered`,`vaccine`,`tx_date`,`tx_host`,`tx_update`) VALUES ( NEW.id_day_municipality,NEW.id_municipality,NEW.date,NEW.new_cases,NEW.actives,NEW.deaths,NEW.recovered,NEW.vaccine,NOW(),NEW.tx_host,NOW());
 END;
 |
 DELIMITER ;
 
 DELIMITER |
-CREATE TRIGGER tg_update_total_municipio
-    AFTER UPDATE ON day_municipio
+CREATE TRIGGER tg_update_total_municipality
+    AFTER UPDATE ON day_municipality
     FOR EACH ROW
 BEGIN
     UPDATE DEPARTMENT
